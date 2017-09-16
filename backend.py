f3ab1458fd8f11d5f53e3b926de60f13c2964d62
coolcom200 <leonfattakhov@yahoo.com>
2017-09-16 11:51:30 -0400

    updated server to work with back end and update js to properly work with ajax

--- backend.py
+++ backend.py
@@ -1,11 +1,15 @@
 import json
-
+from flask_cors import CORS
 from flask import Flask, request
 from hashlib import sha256
 
 app = Flask(__name__)
+CORS(app)
 success = dict()
 success["result"] = "true"
+
+fail = dict()
+fail["result"] = "true"
 
 
 @app.route('/r', methods=['POST'])
@@ -15,10 +19,11 @@ def show_user_profile():
     user = data["username"]
     password = data["password"]
     # TODO check if user is in db
+    # if (not in db):
+    # return
     # TODO encrypt with sha256 then store to db
     hashedPass = sha256(password.encode()).hexdigest()
     print(hashedPass)
-
     return json.dumps(success)
 
 
@@ -31,7 +36,7 @@ def sign_in():
     # TODO Get users password hashed and compare to the hash
     storedPass = ""
     if storedPass == sha256(bytes(password)).hexdigest():
-        return '{result:"success"}'
+        return json.dumps(success)
 
 
 if __name__ == '__main__':
