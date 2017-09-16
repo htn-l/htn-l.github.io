f07f4aa0fe9fa242bec5a40b06fa09e73347422a
coolcom200 <leonfattakhov@yahoo.com>
2017-09-16 11:13:22 -0400

    added server registration

--- backend.py
+++ backend.py
@@ -1,22 +1,38 @@
-from flask import Flask, request
+import json
 
+from flask import Flask, request
+from hashlib import sha256
 
 app = Flask(__name__)
-
-@app.route("/", methods=['GET','POST'])
-def hello():
-    return "Hello World!"
+success = dict()
+success["result"] = "true"
 
 
-@app.route('/register', methods=['GET','POST'])
+@app.route('/r', methods=['POST'])
 def show_user_profile():
+    data = request.form
+    print(data)
+    user = data["username"]
+    password = data["password"]
+    # TODO check if user is in db
+    # TODO encrypt with sha256 then store to db
+    hashedPass = sha256(password.encode()).hexdigest()
+    print(hashedPass)
+
+    return json.dumps(success)
+
+
+@app.route('/signin', methods=['POST'])
+def sign_in():
     data = request.form
     print(data)
     user = data["user"]
     password = data["password"]
-    # TODO check if user is in db
-    # TODO encrypt with sha256 then store to db
-    return "HELLOEOIJAJSLFJAOIFJA"
+    # TODO Get users password hashed and compare to the hash
+    storedPass = ""
+    if storedPass == sha256(bytes(password)).hexdigest():
+        return '{result:"success"}'
+
 
 if __name__ == '__main__':
     app.run(use_reloader=True, debug=True)
